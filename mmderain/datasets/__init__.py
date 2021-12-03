ddc599285b9586729393db7e71ed92a97eb3825c
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2021-12-03 15:42:24 +0800

    add DerainMultipleLQDataset

--- mmderain/datasets/__init__.py
+++ mmderain/datasets/__init__.py
@@ -4,10 +4,11 @@ from .builder import build_dataloader, build_dataset
 from .dataset_wrappers import RepeatDataset
 from .derain_paired_dataset import DerainPairedDataset
 from .derain_unpaired_dataset import DerainUnpairedDataset
+from .derain_multiple_lq_dataset import DerainMultipleLQDataset
 from .registry import DATASETS, PIPELINES
 
 __all__ = [
     'DATASETS', 'PIPELINES', 'build_dataset', 'build_dataloader',
     'BaseDataset', 'RepeatDataset', 'BaseDerainDataset', 'DerainPairedDataset',
-    'DerainUnpairedDataset'
+    'DerainUnpairedDataset', 'DerainMultipleLQDataset'
 ]
