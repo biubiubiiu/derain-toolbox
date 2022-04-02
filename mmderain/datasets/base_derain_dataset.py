0c0bf4c113a19ea4fb517a09e76d0414b8c8a83c
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2022-04-03 02:12:02 +0800

    skip length check in `dataset.evaluate`

--- mmderain/datasets/base_derain_dataset.py
+++ mmderain/datasets/base_derain_dataset.py
@@ -52,9 +52,6 @@ class BaseDerainDataset(BaseDataset):
         """
         if not isinstance(results, list):
             raise TypeError(f'results must be a list, but got {type(results)}')
-        assert len(results) == len(self), (
-            'The length of results is not equal to the dataset len: '
-            f'{len(results)} != {len(self)}')
 
         results = [res['eval_result'] for res in results]  # a list of dict
         eval_result = defaultdict(list)  # a dict of list
@@ -62,14 +59,10 @@ class BaseDerainDataset(BaseDataset):
         for res in results:
             for metric, val in res.items():
                 eval_result[metric].append(val)
-        for metric, val_list in eval_result.items():
-            assert len(val_list) == len(self), (
-                f'Length of evaluation result of {metric} is {len(val_list)}, '
-                f'should be {len(self)}')
 
         # average the results
         eval_result = {
-            metric: sum(values) / len(self)
+            metric: sum(values) / len(values)
             for metric, values in eval_result.items()
         }
 
--- mmderain/datasets/dataset_wrappers.py
+++ mmderain/datasets/dataset_wrappers.py
@@ -1,5 +1,4 @@
 import itertools
-from collections import defaultdict
 
 from mmcv.utils import is_tuple_of, to_2tuple
 from PIL import Image
@@ -92,36 +91,5 @@ class ExhaustivePatchDataset:
         """
         return len(self.patch_infos)
 
-    def evaluate(self, results):
-        """Evaluating with saving generated images. (needs no metrics)
-
-        Args:
-            results (list[tuple]): The output of forward_test() of the model.
-
-        Return:
-            dict: Evaluation results dict
-        """
-        if not isinstance(results, list):
-            raise TypeError(f'results must be a list, but got {type(results)}')
-        assert len(results) == len(self), (
-            'The length of results is not equal to the dataset len: '
-            f'{len(results)} != {len(self)}')
-
-        results = [res['eval_result'] for res in results]  # a list of dict
-        eval_result = defaultdict(list)  # a dict of list
-
-        for res in results:
-            for metric, val in res.items():
-                eval_result[metric].append(val)
-        for metric, val_list in eval_result.items():
-            assert len(val_list) == len(self), (
-                f'Length of evaluation result of {metric} is {len(val_list)}, '
-                f'should be {len(self)}')
-
-        # average the results
-        eval_result = {
-            metric: sum(values) / len(self)
-            for metric, values in eval_result.items()
-        }
-
-        return eval_result
+    def evaluate(self, *args, **kwargs):
+        return self.dataset.evaluate(*args, **kwargs)
