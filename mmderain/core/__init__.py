d73d865cb5e60d5bffc9a14a610146ee3e733da0
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2022-07-16 03:59:17 +0800

    support MAE metric
    cherry pick from https://github.com/open-mmlab/mmediting/pull/871

--- mmderain/core/__init__.py
+++ mmderain/core/__init__.py
@@ -1,5 +1,5 @@
-from .evaluation import (DistEvalHook, EvalHook, L1Evaluation, mse, psnr,
-                         reorder_image, sad, ssim)
+from .evaluation import (DistEvalHook, EvalHook, L1Evaluation, mae,
+                         mse, psnr, reorder_image, sad, ssim)
 from .hooks import RLNetHyperParamAdjustmentHook
 from .initializers import ECNetInitializer
 from .misc import crop_border, tensor2img
@@ -7,8 +7,8 @@ from .optimizer import build_optimizers
 from .scheduler import LinearLrUpdaterHook
 
 __all__ = [
-    'DistEvalHook', 'EvalHook', 'L1Evaluation', 'mse',
-    'psnr', 'reorder_image', 'sad', 'ssim',
-    'RLNetHyperParamAdjustmentHook', 'ECNetInitializer', 'crop_border', 'tensor2img',
-    'build_optimizers', 'LinearLrUpdaterHook'
+    'DistEvalHook', 'EvalHook', 'L1Evaluation', 'mae',
+    'mse', 'psnr', 'reorder_image', 'sad',
+    'ssim', 'RLNetHyperParamAdjustmentHook', 'ECNetInitializer', 'crop_border',
+    'tensor2img', 'build_optimizers', 'LinearLrUpdaterHook'
 ]
--- mmderain/core/evaluation/__init__.py
+++ mmderain/core/evaluation/__init__.py
@@ -1,9 +1,9 @@
 from .eval_hooks import DistEvalHook, EvalHook
-from .metrics import (L1Evaluation, connectivity, gradient_error, mse, niqe,
-                      psnr, reorder_image, sad, ssim)
+from .metrics import (L1Evaluation, connectivity, gradient_error, mae, mse,
+                      niqe, psnr, reorder_image, sad, ssim)
 
 __all__ = [
     'DistEvalHook', 'EvalHook', 'L1Evaluation', 'connectivity',
-    'gradient_error', 'mse', 'niqe', 'psnr',
-    'reorder_image', 'sad', 'ssim'
+    'gradient_error', 'mae', 'mse', 'niqe',
+    'psnr', 'reorder_image', 'sad', 'ssim'
 ]
